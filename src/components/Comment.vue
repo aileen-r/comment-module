f9ac2a2a7df4a00bbf0ec918ad698f59dc26a943
Aileen Rae <aileenlrae@gmail.com>
2021-05-14 17:27:25 +0100

    Remove margin-bottom from last comment

--- src/components/Comment.vue
+++ src/components/Comment.vue
@@ -1,7 +1,12 @@
 <template>
-  <details open class="comment" :id="comment.id">
+  <details
+    open
+    class="comment"
+    :class="{ 'nested-comment': nested }"
+    :id="comment.id"
+  >
     <a :href="`#${comment.id}`" class="comment-border-link">
-      <span class="sr-only">Jump to comment {{ comment.id }}</span>
+      <span class="sr-only">Jump to {{ comment.author }}'s</span>
     </a>
     <summary>
       <div class="comment-heading">
@@ -30,151 +35,10 @@
         v-for="reply in comment.replies"
         :key="reply.id"
         :comment="reply"
+        :nested="true"
       />
     </div>
-
-    <!-- <div class="replies">
-
-
-
-      <details open class="comment" id="comment-3">
-        <a href="#comment-3" class="comment-border-link">
-          <span class="sr-only">Jump to comment-3</span>
-        </a>
-        <summary>
-          <div class="comment-heading">
-            <div class="comment-voting">
-              <button type="button">
-                <span aria-hidden="true">&#9650;</span>
-                <span class="sr-only">Vote up</span>
-              </button>
-              <button type="button">
-                <span aria-hidden="true">&#9660;</span>
-                <span class="sr-only">Vote down</span>
-              </button>
-            </div>
-            <div class="comment-info">
-              <a href="#" class="comment-author">2edgy4u</a>
-              <p class="m-0">-19 points &bull; 3 days ago</p>
-            </div>
-          </div>
-        </summary>
-
-        <div class="comment-body">
-          <p>
-            Wow, are you serious? You have some pretty low standards to be able
-            to enjoy this. Good for you I guess, but you should honestly stop
-            making these embarrassing comments.
-          </p>
-          <button type="button">Reply</button>
-          <button type="button">Report</button>
-        </div>
-
-        <div class="replies">
-          <details open class="comment" id="comment-4">
-            <a href="#comment-4" class="comment-border-link">
-              <span class="sr-only">Jump to comment-4</span>
-            </a>
-            <summary>
-              <div class="comment-heading">
-                <div class="comment-voting">
-                  <button type="button">
-                    <span aria-hidden="true">&#9650;</span>
-                    <span class="sr-only">Vote up</span>
-                  </button>
-                  <button type="button">
-                    <span aria-hidden="true">&#9660;</span>
-                    <span class="sr-only">Vote down</span>
-                  </button>
-                </div>
-                <div class="comment-info">
-                  <a href="#" class="comment-author">modpowertrip</a>
-                  <p class="m-0">9 points &bull; 2 days ago</p>
-                </div>
-              </div>
-            </summary>
-
-            <div class="comment-body">
-              <p>
-                You are breaking <a href="#rule-687">Rule #687</a> with that
-                comment. Please avoid posting like this in the future, or I will
-                ban you.
-              </p>
-              <button type="button">Reply</button>
-              <button type="button">Report</button>
-            </div>
-          </details>
-
-
-          <details open class="comment" id="comment-5">
-            <a href="#comment-5" class="comment-border-link">
-              <span class="sr-only">Jump to comment-5</span>
-            </a>
-            <summary>
-              <div class="comment-heading">
-                <div class="comment-voting">
-                  <button type="button">
-                    <span aria-hidden="true">&#9650;</span>
-                    <span class="sr-only">Vote up</span>
-                  </button>
-                  <button type="button">
-                    <span aria-hidden="true">&#9660;</span>
-                    <span class="sr-only">Vote down</span>
-                  </button>
-                </div>
-                <div class="comment-info">
-                  <a href="#" class="comment-author">imemespam</a>
-                  <p class="m-0">3 points &bull; 2 days ago</p>
-                </div>
-              </div>
-            </summary>
-
-            <div class="comment-body">
-              <p>Well, that's just like your opinion man.</p>
-              <button type="button">Reply</button>
-              <button type="button">Report</button>
-            </div>
-          </details>
-
-
-          <details open class="comment" id="comment-6">
-            <a href="#comment-6" class="comment-border-link">
-              <span class="sr-only">Jump to comment-6</span>
-            </a>
-            <summary>
-              <div class="comment-heading">
-                <div class="comment-voting">
-                  <button type="button">
-                    <span aria-hidden="true">&#9650;</span>
-                    <span class="sr-only">Vote up</span>
-                  </button>
-                  <button type="button">
-                    <span aria-hidden="true">&#9660;</span>
-                    <span class="sr-only">Vote down</span>
-                  </button>
-                </div>
-                <div class="comment-info">
-                  <a href="#" class="comment-author">lukerbro57</a>
-                  <p class="m-0">0 points &bull; 2 days ago</p>
-                </div>
-              </div>
-            </summary>
-
-            <div class="comment-body">
-              <p>Lol I agree with you.</p>
-              <button type="button">Reply</button>
-              <button type="button">Report</button>
-            </div>
-          </details>
-
-
-          <a href="#load-more">Load more replies</a>
-        </div>
-      </details>
-
-    </div> -->
   </details>
-  <!-- Comment 1 end -->
 </template>
 
 <script>
@@ -196,6 +60,10 @@ export default {
       type: Object,
       default: () => ({}),
     },
+    nested: {
+      type: Boolean,
+      default: false,
+    },
   },
 };
 </script>
@@ -210,6 +78,10 @@ $reply-offset-md: calc(#{$gutter-width-md} + #{$img-width-md});
 
 .comment {
   position: relative;
+
+  &:not(.nested-comment):last-child .comment-body {
+    margin-bottom: 0;
+  }
 }
 
 .comment-heading {
